c5af10e77082b2780d201d68e49269b9c183c523
Swarzstein <zorrillagja@gmail.com>
2023-04-01 12:11:35 -0400

    "tests for factorial method created"

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,1 +1,16 @@
-require_relative 'classes/solver'
+require_relative '../classes/solver'
+
+describe Solver do
+  solver = Solver.new
+  context "When testing factorial method" do
+    it "Input of 5 returns 120" do
+      expect(solver(5)).to eq 120
+    end
+    it "Input of 0 returns 1" do
+      expect(solver(0)).to eq 1
+    end
+    it "Input of negative number raises an exception" do
+      expect(solver(-5)).to raise_error()
+    end
+  end
+end
