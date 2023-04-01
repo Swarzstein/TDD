f063ed12e3425f162554a765fa4da533f7bc2ba3
Israel <chizunguisrael@gmail.com>
2023-04-01 20:38:22 +0200

    Define fizzbuz function

--- classes/solver.rb
+++ classes/solver.rb
@@ -15,7 +15,16 @@ class Solver
     end
     reversed
   end
+
   def fizzbuzz n
-    string
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
+    end
   end
 end
