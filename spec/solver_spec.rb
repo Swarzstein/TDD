524fad9b7194b0cb4d020a7f0f52cbe68e576635
Swarzstein <zorrillagja@gmail.com>
2023-04-01 12:16:08 -0400

    bug corrected

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,13 +4,13 @@ describe Solver do
   solver = Solver.new
   context "When testing factorial method" do
     it "Input of 5 returns 120" do
-      expect(solver(5)).to eq 120
+      expect(solver.factorial(5)).to eq 120
     end
     it "Input of 0 returns 1" do
-      expect(solver(0)).to eq 1
+      expect(solver.factorial(0)).to eq 1
     end
     it "Input of negative number raises an exception" do
-      expect(solver(-5)).to raise_error()
+      expect(solver.factorial(-5)).to raise_error()
     end
   end
 end
