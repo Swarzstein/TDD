283724cca7e7c22c04ca37c57a396d4c5658ca79
Swarzstein <zorrillagja@gmail.com>
2023-04-01 12:27:28 -0400

    factorial method of solver created

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,11 +1,17 @@
 class Solver
-  def factorial N
-    f
+  def factorial n
+    if n > 1
+      n * factorial(n - 1)
+    elsif n == 0 || n == 1
+      1
+    else
+      raise ArgumentError, "Negative Integer #{n} detected, expected 0 or positive integer"
+    end
   end
   def reverse word
     word
   end
-  def fizzbuzz N
+  def fizzbuzz n
     string
   end
 end
